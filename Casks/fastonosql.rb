07ad81598eed382eccd9714c6885c4119e73d0b3
Ricardo Vega <ricveal@gmail.com>
2019-02-08 14:36:12 +0100

    Update Fastosql version
    Increased minor version of FastoSQL

--- Casks/fastonosql.rb
+++ Casks/fastonosql.rb
@@ -1,5 +1,5 @@
 cask 'fastonosql' do
-  version '1.26.0'
+  version '1.26.2'
   sha256 '771893c53c1a523daf9cc70f378075b49b854a5ced1cbc7d926a1cf9247dda17'
 
   url "https://fastonosql.com/downloads_pro/macosx/fastonosql_pro-#{version}-x86_64.dmg"
